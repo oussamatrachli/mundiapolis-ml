15b2284c45a20d8baeda93899f102cd9bb777d33
Oussama Trachli <76519963+oussamatrachli@users.noreply.github.com>
2021-03-22 19:36:51 +0100

    Create 0-neuron.py

---
+++ 0x01-classification/0-neuron.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Neuron:
+    def __init__(self,nx):
+        if (not(isinstance(nx,int))):
+            raise TypeError('nx must be an integer')
+        if(nx<1):
+            raise ValueError('nx must be a positive integer')
+        self.W = np.random.normal()
+        self.b = 0
+        self.A = 0
