16aa5449856db7d77aaded82d9073c329f12096f
Oussama Trachli <76519963+oussamatrachli@users.noreply.github.com>
2021-03-26 23:22:30 +0100

    Update 1-neuron.py

--- 0x01-classification/1-neuron.py
+++ 0x01-classification/1-neuron.py
@@ -1,11 +1,22 @@
 import numpy as np
-
 class Neuron:
-    def __init__(self,nx):
-        if (not(isinstance(nx,int))):
+    def __init__(self, nx):
+        if type(nx) is not int:
             raise TypeError('nx must be an integer')
-        if(nx<1):
+        if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.__W = np.random.normal()
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
