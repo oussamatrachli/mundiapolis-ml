cffb80e4fc90af987aaac6f8fabdfd16dbb1b851
Oussama Trachli <76519963+oussamatrachli@users.noreply.github.com>
2021-04-03 10:27:10 +0100

    Create 12-neural_network.py

---
+++ 0x01-classification/12-neural_network.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+class NeuralNetwork():
+
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nx, nodes).reshape(nodes, nx)
+        self.__b1 = np.zeros(nodes).reshape(nodes, 1)
+        self.__A1 = 0
+        self.__W2 = np.random.randn(nodes).reshape(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        sigmoid_1 = 1 / (1 + np.exp(-Z1))
+        self.__A1 = sigmoid_1
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        sigmoid_2 = 1 / (1 + np.exp(-Z2))
+        self.__A2 = sigmoid_2
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+
+        m = Y.shape[1]
+        C = - (1 / m) * np.sum(
+            np.multiply(
+                Y, np.log(A)) + np.multiply(
+                1 - Y, np.log(1.0000001 - A)))
+        return C
+
+    def evaluate(self, X, Y):
+
+        self.forward_prop(X)
+        cost = self.cost(Y, self.__A2)
+        prediction = np.where(self.__A2 >= 0.5, 1, 0)
+        return prediction, cost
